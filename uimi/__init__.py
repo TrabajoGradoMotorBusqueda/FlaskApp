3fcc0ebd8e9d2a0101394848e5907d622b885482
David Pinchao <sangeeky@outlook.com>
2021-05-18 19:58:57 -0500

    Añadir campos adicionales en resultados

--- uimi/__init__.py
+++ uimi/__init__.py
@@ -57,5 +57,52 @@ def search_engine(palabras):
     #             ranking_final_documentos.append(ontologia_doc)
     #             ranking_final_documentos.append(d2v_doc)
 
+    # construir_resultados(ranking_final_documentos)
+
     return ranking_final_documentos
     # return ranking_final_documentos
+
+#
+# def construir_resultados(lista_resultados):
+#     resultados = {}
+#     for i, proyecto in enumerate(lista_resultados):
+#         id_proyecto = proyecto.get_id_proyecto_investigacion()[0]  # atributo
+#         titulo = proyecto.get_titulo_proyecto_investigacion()[0]  # atributo
+#         resumen = proyecto.get_resumen_proyecto_investigacion()[0]  # atributo
+#         palabras_clave = proyecto.get_palabras_clave()  # atributo en lista
+#         facultades = proyecto.pi_se_asocia_facultad  # instancia
+#         programas = proyecto.pi_se_asocia_programa  # instancia
+#         lineas_investigacion = proyecto.pi_pertenece_li  # instancia
+#         grupos_investigacion = [linea.li_pertenece_gi[0] for linea in lineas_investigacion]  # instancia
+#         viis = proyecto.pi_pertenece_viis.get_nombre_VIIS()[0]  # atributo
+#         universidad = proyecto.pi_pertenece_viis.viis_pertenece_universidad.get_nombre_universidad()[0] # atributo
+#         convocatoria = proyecto.pi_pertenece_convocatoria  # instancia
+#         tipo_convocatoria = convocatoria.get_tipo_convocatoria()[0]  # atributo
+#         anio_convocatoria = convocatoria.get_anio_convocatoria()[0]  # atributo
+#         tipo_investigacion = proyecto.get_tipo_proyecto_investigacion()[0]  # atributo
+#         if tipo_investigacion is "Docente":
+#             autores = proyecto.pi_tiene_autor_docente  # instancias
+#         else:
+#             autores = proyecto.pi_tiene_autor_estudiante  # instancias
+#         asesores = proyecto.pi_es_asesorado_docente  # instancias
+#
+#         resultados[i] = {
+#             "id": id_proyecto,
+#             "tipo_investigacion": tipo_investigacion,
+#             "titulo": titulo,
+#             "resumen": resumen,
+#             "palabras_clave": palabras_clave,
+#             "autores": [autor.get_nombres_investigador + " " + autor.get_apellidos_investigador for autor in autores],
+#             "asesores": [asesor.get_nombres_investigador + " " + asesor.get_apellidos_investigador for asesor in asesores],
+#             "universidad": universidad,
+#             "facultades": [facultad.get_nombre_facultad()[0] for facultad in facultades],
+#             "programas": [programa.get_nombre_programa()[0] for programa in programas],
+#             "grupos_investigacion": [grupo.get_nombre_grupo_investigacion()[0] for grupo in grupos_investigacion],
+#             "lineas_investigacion": [linea.get_nombre_linea_investigacion()[0] for linea in lineas_investigacion],
+#             "viis": viis,
+#             "tipo_convocatoria": tipo_convocatoria,
+#             "anio_convocatoria": anio_convocatoria
+#         }
+#
+#     # nombres_investigador, apellidos_investigador
+#     return resultados
